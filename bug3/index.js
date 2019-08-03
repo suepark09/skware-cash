26a8b0c64201932aa8e40a32b4065072fe903dbf
Sue Park <suepandrice06@gmail.com>
2019-08-03 13:42:30 -0500

    finished example 3

--- bug3/index.js
+++ bug3/index.js
@@ -3,6 +3,8 @@
 //   given a list of transactions, will generate an HTML
 //   string representing the transactions
 //*******************************************************
+
+
 function renderTransactions(transactions) {
 	var finalHTML = '<div class="buffer">TRANSACTIONS</div>';
 
